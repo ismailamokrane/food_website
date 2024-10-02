322362b9a75eb5e2a2f446d7bc313179878cd07b
ismail <mr.amokrane@gmail.com>
2024-10-02 23:16:02 +0100

    création du menu

--- header.php
+++ header.php
@@ -1,1 +1,9 @@
-<h1>Voice le header</h1>
+<nav class="bg-white shadow p-4">
+    <ul class="flex justify-center space-x-4">
+        <li><a href="#" class="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-lg">Accueil</a></li>
+        <li><a href="#" class="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-lg">Vidéos</a></li>
+        <li><a href="#" class="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-lg">Articles</a></li>
+        <li><a href="#" class="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-lg">Contactez-nous</a></li>
+    </ul>
+</nav>
+
